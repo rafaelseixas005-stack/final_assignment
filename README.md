fd90e83d7599755d9965719fa91a9efb85bb2cfc
rafa <rafaelseixas005@gmail.com>
2026-01-04 00:16:06 +0000

    add tudo

---
+++ README.md
@@ -0,0 +1,23 @@
+Note: this repo stores scripts for students to play with (several errors can be found in the scripts, i.e., credentials)
+
+*Final assignment*
+
+team:  max group of 3 people
+
+Delivery date: 9 January 2026
+
+Exercise 1
+1. Fork this repository
+2. in the new repositry, make sure that:
+   a) a proper index.html file is served by NGINX
+   b) make sure passwords are not shown in the Terraform and Ansbile files (they are not visible in the repository)
+  
+
+Exercise 2:
+1. Create a new repository
+2. Create a Linux machine in Azure that will play a DHCP role (test the configurarion)
+3. use Terraform and Ansible (zero manual configuration)
+4. make sure that no passwords are stored in scripts
+5. delivery the code in a github repo
+
+TIP: https://techcommunity.microsoft.com/blog/azurenetworkingblog/custom-dhcp-support-in-azure/4089674
---
+++ main.tf
@@ -0,0 +1,126 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Use a version constraint appropriate for your project
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "my-terraform-rg"
+  location = "francecentral"
+}
+
+# 2. Virtual Network and Subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# 3. Public IP Address
+resource "azurerm_public_ip" "publicip" {
+  name                = "my-vm-publicip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# 4. Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "my-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# 5. Network Security Group (to allow SSH)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "my-vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "ssh_rule" {
+  name                        = "SSH_Access"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22" # Standard SSH port
+  source_address_prefix       = "*"  # Allow from any IP (Be cautious! Restrict this in production)
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_network_security_group.nsg.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "http_rule" {
+  name                        = "HTTP_Access"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_network_security_group.nsg.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+# 6. The Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "my-ubuntu-vm"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  size                            = "Standard_B2S" # Basic VM size
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  disable_password_authentication = false
+  admin_username                  = "azureuser"
+  admin_password                  = var.admin_password
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# Output the public IP to connect to the VM later
+output "public_ip_address" {
+  value = azurerm_public_ip.publicip.ip_address
+}
